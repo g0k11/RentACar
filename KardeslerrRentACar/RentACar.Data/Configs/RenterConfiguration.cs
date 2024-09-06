a57cad3d84d84f16c079590603f54b8cb9a6b27f
erayozden167 <erayozden167@gmail.com>
2024-09-07 00:36:58 +0300

    .

---
+++ KardeslerrRentACar/RentACar.Data/Configs/RenterConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RentACar.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Data.Configs
+{
+    public class RenterConfiguration : IEntityTypeConfiguration<Renter>
+    {
+        public void Configure(EntityTypeBuilder<Renter> builder)
+        {
+            builder.HasOne(r => r.User)   
+               .WithOne()                    
+               .HasForeignKey<Renter>(r => r.UserId) 
+               .OnDelete(DeleteBehavior.Cascade);     
+        }
+    }
+}
