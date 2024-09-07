75c18be83cbbfa09bb5d96c22246833a8e8c48e2
Gokberk Seckin <gokberks92@gmail.com>
2024-09-07 03:43:12 +0300

    updated

--- KardeslerrRentACar/RentACar.Data/Configs/RenterConfiguration.cs
+++ KardeslerrRentACar/RentACar.Data/Configs/RenterConfiguration.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RentACar.Core.Interfaces;
 using RentACar.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +18,20 @@ namespace RentACar.Data.Configs
             builder.HasOne(r => r.User)   
                .WithOne()                    
                .HasForeignKey<Renter>(r => r.UserId) 
-               .OnDelete(DeleteBehavior.Cascade);     
+               .OnDelete(DeleteBehavior.Cascade);
+           
+        }
+      
+    }
+    public class ReservationConfiguration : IEntityTypeConfiguration<Reservations>
+    {
+        public void Configure(EntityTypeBuilder<Reservation> builder)
+        {
+            // Configuring the one-to-many relationship between Renter and Reservations
+            builder.HasOne(res => res.Renter)        // Reservation has one Renter
+                   .WithMany(r => r.Reservations)    // Renter has many Reservations
+                   .HasForeignKey(res => res.RenterId) // Foreign key in Reservation is RenterId
+                   .OnDelete(DeleteBehavior.Cascade);  // Cascade delete when a Renter is deleted
         }
     }
 }
