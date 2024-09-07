1b794ab231f87601784832cc99aac169d66f1d97
erayozden167 <erayozden167@gmail.com>
2024-09-07 20:53:14 +0300

    sql
    connection and seed

--- KardeslerrRentACar/RentACar.Application/Services/AuthService.cs
+++ KardeslerrRentACar/RentACar.Application/Services/AuthService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RentACar.Application.Interfaces;
+using RentACar.DTOs.Auth;
+using RentACar.Infrastructure.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,22 @@ using System.Threading.Tasks;
 
 namespace RentACar.Application.Services
 {
-    public class AuthService
+    public class AuthService : IAuthService
     {
+        private readonly IRenterRepository _renterRepository;
+        public AuthService(IRenterRepository renterRepository)
+        {
+            _renterRepository = renterRepository;
+        }
+
+        public async Task<AuthResultDto> LoginAsync(LoginDTO login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RegisterAsync(RegisterDTO addRenter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
